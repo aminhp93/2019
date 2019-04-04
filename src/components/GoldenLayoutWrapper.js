25f01bb0093b243a4a978213669b253306efa5be
Minh Pham <minh.pham@quant-edge.com>
2019-04-04 10:09:07 +0700

    Change config

--- src/components/GoldenLayoutWrapper.js
+++ src/components/GoldenLayoutWrapper.js
@@ -7,7 +7,6 @@ import PropTypes from 'prop-types';
 import Self from './Self';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import layoutConfig from '../layoutConfig';
-import axios from 'axios';
 import dataStorage from '../dataStorage';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -16,6 +15,9 @@ import * as checkFilterReady from '../actions/checkFilterReady.actions';
 window.React = React;
 window.ReactDOM = ReactDOM;
 
+const dicComponent = {
+    Self: Self
+};
 class GoldenLayoutWrapper extends React.Component {
     constructor(props) {
         super(props);
@@ -27,82 +29,113 @@ class GoldenLayoutWrapper extends React.Component {
         this.initGoldenLayout = this.initGoldenLayout.bind(this);
     }
 
+    wrapComponent(Component, store) {
+        class Wrapped extends React.Component {
+            ref(dom) {
+                if (dom) dom.react = this;
+            }
+
+            render() {
+                return (
+                    <Provider store={store}>
+                        <div className={'wrapComponent'} ref={this.ref.bind(this)}>
+                            <Component {...this.props} />
+                        </div>
+                    </Provider>
+                );
+            }
+        }
+        return Wrapped;
+    }
+
+    initLayoutConfig(lst = []) {
+        Array.isArray(lst) && lst.map(item => {
+            if (item.content) {
+                this.initLayoutConfig(item.content);
+            }
+            if (item.type === 'component') {
+                this.registerComponent(item);
+            }
+        });
+    }
 
     initGoldenLayout(mode) {
-        new Promise((resolve) => {
-            let config = {
-                content: []
-            };
-            let layout = layoutConfig.getDefaultLayout();
-            const cbFunc = layout => {
-                config = {
-                    dimensions: {
-                        // headerHeight: 32,
-                        // borderWidth: 8,
-                        // minItemHeight: 192,
-                        // minItemWidth: 300
-                    },
-                    content: layout
-                }
-                function wrapComponent(Component, store) {
-                    class Wrapped extends React.Component {
-                        ref(dom) {
-                            if (dom) dom.react = this;
-                        }
+        let layout = layoutConfig.getDefaultLayout();
+        const config = {
+            dimensions: {
+                // headerHeight: 32,
+                // borderWidth: 8,
+                // minItemHeight: 192,
+                // minItemWidth: 300
+            },
+            content: layout
+        }
 
-                        render() {
-                            return (
-                                <Provider store={store}>
-                                    <div className={'wrapComponent'} ref={this.ref.bind(this)}>
-                                        <Component {...this.props} />
-                                    </div>
-                                </Provider>
-                            );
-                        }
-                    }
-                    return Wrapped;
-                }
 
-                if (this.goldenLayout) this.goldenLayout.destroy();
+        if (this.goldenLayout) this.goldenLayout.destroy();
 
-                this.goldenLayout = new GoldenLayout(config, this.layout);
-                const listComponent = [
-                    ['Self', Self]
-                ]
-                for (let i = 0; i < listComponent.length; i++) {
-                    this.goldenLayout.registerComponent(listComponent[i][0],
-                        wrapComponent(listComponent[i][1], this.context.store)
-                    );
-                }
-
-                /// Callback for every created stack
-                this.goldenLayout.on('stackCreated', (stack) => {
-
-                });
-
-                this.goldenLayout.on('tabCreated', (tabContainer) => {
-
-                });
-
-                this.goldenLayout.on('componentCreated', com => {
-
-                })
-
-                this.goldenLayout.init();
-
-                this.goldenLayout.on('stateChanged', (event) => {
-
-                });
-
-                window.addEventListener('resize', () => {
-                    this.goldenLayout.updateSize();
-                });
-                resolve()
-            }
-            cbFunc(layout)
-        }).then(() => {
-            //
+        this.goldenLayout = new GoldenLayout(config, this.layout);
+        this.initLayoutConfig([config]);
+        const listComponent = [
+            ['Self', Self]
+        ]
+        /// Callback for every created stack
+        this.goldenLayout.on('stackCreated', (stack) => {
+            console.log(123)
         });
+
+        this.goldenLayout.on('tabCreated', (tabContainer) => {
+            console.log(123)
+        });
+
+        this.goldenLayout.on('componentCreated', com => {
+            console.log(123)
+        })
+        this.goldenLayout.on('itemDestroyed', (event) => {
+            console.log(event)
+        });
+        this.goldenLayout.on('itemCreated', (event) => {
+            console.log(123)
+        });
+
+        this.goldenLayout.on('stateChanged', (event) => {
+            console.log(event)
+        });
+
+        this.goldenLayout.on('rowCreated', (event) => {
+            console.log(123)
+        });
+        this.goldenLayout.on('columnCreated', (event) => {
+            console.log(123)
+        });
+        this.goldenLayout.on('selectionChanged', (event) => {
+            console.log(123)
+        });
+        this.goldenLayout.on('windowOpened', (event) => {
+            console.log(event)
+        });
+        this.goldenLayout.on('windowClosed', (event) => {
+            console.log(event)
+        });
+        if (this.goldenLayout.config && this.goldenLayout.config.content && this.goldenLayout.config.content.length === 1) {
+            if (this.goldenLayout.config.content[0].type === 'component') {
+                this.registerComponent(this.goldenLayout.config.content[0])
+            }
+        }
+        this.goldenLayout.init();
+
+        window.addEventListener('resize', () => {
+            this.goldenLayout.updateSize();
+        })
+    }
+
+    registerComponent(item) {
+        if (!this.goldenLayout.existed) this.goldenLayout.existed = {};
+        if (this.goldenLayout.existed[item.component]) return;
+        this.goldenLayout.existed[item.component] = true;
+        this.goldenLayout.registerComponent(item.component,
+            this.wrapComponent(dicComponent[item.component], this.context.store)
+        )
     }
 
     addComponentToStack(index, state = {}) {
--- src/components/Self/Self.js
+++ src/components/Self/Self.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 class Self extends React.Component {
+    constructor(props) {
+        super(props)
+        console.log(props)
+    }
     render() {
         return <div>PHAM NGOC MINH - Amin</div>
     }
